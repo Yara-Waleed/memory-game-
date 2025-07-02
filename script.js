59cd67c4c57c3b09786f0dea8774e03edfb5bca7
Yara Waleed <yarawaleed292@gmail.com>
2025-07-02 22:26:51 +0300

    if they match they stay flipped if not it gets hidden

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ for (let i = 0; i < boxCount; i++) {
   const randomIndex = Math.floor(Math.random() * imgsPickList.length) //picks a random index from the array
   const color = imgsPickList[randomIndex] //we get the color from the index
 }
+
 //to loop through the list
 const cardElements = []
 const cardImgs = []
@@ -42,15 +43,20 @@ function flipCards(index) {
   console.log(flippedCards)
   const img = cardImgs[index]
   event.target.setAttribute('style', `background-color: ${img}`) // event.target finds the HTML element <div> we need to style
+
   if (flippedCards.length === 2) {
     let card1 = cardElements[flippedCards[0]]
     console.log(card1.getAttribute('style'))
     let card2 = cardElements[flippedCards[1]]
     console.log(card2.getAttribute('style'))
     if (card1.getAttribute('style') === card2.getAttribute('style')) {
-      console.log('match')
+      flippedCards.length = 0 //it resets the indexes so we move to getting another two cards macthed
     } else {
-      console.log('not a match')
+      setTimeout(() => {
+        card1.removeAttribute('style') //so it removes the style
+        card2.removeAttribute('style')
+        flippedCards.length = 0
+      }, 1000) // the 1000 is to delay the cards flipping back
     }
   }
 }
